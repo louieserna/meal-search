ee2949c6a2d8b5e5e75e1c0913513e80f1027a24
Luis Serna <louieserna44@yahoo.com>
2025-11-20 10:48:04 -0600

    data store and meals by letter complete

--- src/views/MealsByLetter.vue
+++ src/views/MealsByLetter.vue
@@ -1,5 +1,35 @@
 <script setup>
+import { computed, watch } from 'vue'
+import store from '../store'
+import { useRoute } from 'vue-router'
+import Meals from '../composables/Meals.vue'
+
+const route = useRoute()
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+const meals = computed(() => store.state.mealsByLetter)
+
+watch(route, () => {
+    store.dispatch('searchMealsByLetter', route.params.letter)
+})
+
 </script>
 <template>
-    
+    <div class="p-8 pb-0 text-orange-500">
+        <h1 class="text-4xl font-bold mb-4">Search Meals By Letter</h1>
+</div>
+
+<div class="flex justify-center gap-2 mt-2">
+
+<router-link 
+  v-for="letter in letters" 
+  :key="letter"
+  :to="{ name: 'MealsByLetter', params: { letter } }"
+>
+  {{ letter }}
+</router-link>
+</div>
+
+
+
+<Meals :meals="meals"></Meals>
 </template>
